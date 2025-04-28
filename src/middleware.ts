74c2b9855c11629ca5848daba823aecea205c0f3
Navneesh Yadav <yadavnavneesh999@gmail.com>
2025-04-28 19:39:46 +0530

    role based functionality added

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { auth, clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
+
+const isProtectedRoute = createRouteMatcher(['/admin(.*)', '/orders(.*)'])
+
+const isAdminRoute = createRouteMatcher(['/admin(.*)']);
+export default clerkMiddleware(async (auth, req) => {
+    const { userId, redirectToSignIn } = await auth();
+
+    if (isAdminRoute(req) && (await auth()).sessionClaims?.metadata?.role !== 'admin') {
+        const url = new URL("/", req.url);
+        return NextResponse.redirect(url);
+
+    }
+
+    if (!userId && isProtectedRoute(req))
+        return redirectToSignIn();
+});
+
+export const config = {
+    matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Always run for API routes
+        '/(api|trpc)(.*)',
+    ],
+}
